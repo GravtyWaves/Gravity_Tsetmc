48c1c164bcaaa28e092428735ab583ffcdf48f24
GravityMg <GravityWavesManagement@gmail.com>
2025-11-26 12:38:36 +0330

    update: update test.py

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect('tsetmc_data.db')  # نام دیتابیس را درست وارد کن
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print(cursor.fetchall())
-conn.close()
+from gravity_tse import get_price_history
+
+df = get_price_history(stock='خودرو')
+print(df)
